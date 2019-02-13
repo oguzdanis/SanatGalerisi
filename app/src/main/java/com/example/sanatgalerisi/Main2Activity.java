7dcbbc3ee2f9f9a5846877d79d720a7bd3c12adc
Mehmet Oguz Danis <danisoguz@outlook.com>
2019-02-13 15:37:19 +0300

    izin isteme

--- app/src/main/java/com/example/sanatgalerisi/Main2Activity.java
+++ app/src/main/java/com/example/sanatgalerisi/Main2Activity.java
@@ -1,20 +1,55 @@
 package com.example.sanatgalerisi;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class Main2Activity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        imageView = findViewById(R.id.imageView);
     }
+
     public void select(View view){
+        //izin verilmediyse
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+        requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},2);
+        }
+
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent,1);
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if (requestCode==1 && resultCode==RESULT_OK && data!=null){
+            Uri image = data.getData();
+            try{
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),image);
+            }catch (Exception exception){
+                exception.printStackTrace();
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
     public  void save(View view){
 
     }
